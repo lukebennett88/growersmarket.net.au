0f6c0886fa27c115ffb72866b04333e1edef0538
Luke Bennett <hello@lukebennett.com.au>
2021-01-11 11:36:24 +1100

    Add black variant and make position relative

--- src/components/horizontal-padding.tsx
+++ src/components/horizontal-padding.tsx
@@ -4,6 +4,7 @@ enum Variant {
   GRAY,
   GREEN,
   WHITE,
+  BLACK,
   TRANSPARENT,
 }
 
@@ -17,6 +18,7 @@ const VARIANT_MAPS: Record<Variant, string> = {
   [Variant.GRAY]: 'bg-gray-light',
   [Variant.GREEN]: 'bg-green-dark text-white',
   [Variant.WHITE]: 'bg-white',
+  [Variant.BLACK]: 'bg-gray-900 text-white',
   [Variant.TRANSPARENT]: 'bg-transparent',
 };
 
@@ -28,7 +30,7 @@ function HorizontalPadding({
   return React.createElement(
     as || 'div',
     {
-      className: `${VARIANT_MAPS[variant]} px-4 sm:px-6 lg:px-8`,
+      className: `${VARIANT_MAPS[variant]} relative px-4 sm:px-6 lg:px-8`,
     },
     children
   );
