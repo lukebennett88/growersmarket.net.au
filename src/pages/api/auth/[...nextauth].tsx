589fde592a99383034a27c93643b05f7ebc3c394
Luke Bennett <hello@lukebennett.com.au>
2021-01-13 17:30:30 +1100

    Save progress on adding Prisma

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,6 +1,10 @@
 import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const options = {
   // Configure one or more authentication providers
@@ -22,8 +26,31 @@ const options = {
     // }),
   ],
 
+  // Set debug to `true` to enable debug messages for authentication and database operations.
+  debug: process.env.NODE_ENV === 'development',
+
+  // A random string used to hash tokens, sign cookies and generate crytographic keys.
+  secret: process.env.AUTH_SECRET,
+
+  jwt: {
+    // A secret to use for key generation - you should set this explicitly
+    // Defaults to NextAuth.js secret if not explicitly specified.
+    secret: process.env.JWT_SECRET,
+
+    // Set to true to use encryption. Defaults to false (signing only).
+    // encryption: true,
+
+    // You can define your own encode/decode functions for signing and encryption
+    // if you want to override the default behaviour.
+    // encode: async ({ secret, token, maxAge }) => {},
+    // decode: async ({ secret, token, maxAge }) => {},
+  },
+
   // A database is optional, but required to persist accounts in a database
-  // database: process.env.DATABASE_URL,
+  database: process.env.DATABASE_URL,
+
+  // Configure your NextAuth.js to use the Prisma adapter:
+  adapter: Adapters.Prisma.Adapter({ prisma }),
 
   pages: {
     signIn: '/signin',
