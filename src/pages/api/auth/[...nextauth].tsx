e1e673e83acb76620d8451eb2674f721e15936f3
Luke Bennett <hello@lukebennett.com.au>
2021-01-15 13:44:18 +1100

    Comment out debugger

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -24,7 +24,7 @@ const options: InitOptions = {
       from: process.env.EMAIL_FROM,
     }),
   ],
-  debug: process.env.NODE_ENV === 'development',
+  // debug: process.env.NODE_ENV === 'development',
   secret: process.env.AUTH_SECRET,
   jwt: {
     secret: process.env.JWT_SECRET,
