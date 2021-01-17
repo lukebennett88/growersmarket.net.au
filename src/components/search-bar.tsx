4849a6a5f98373452c549d01db5dc720d97d5496
Luke Bennett <hello@lukebennett.com.au>
2021-01-17 15:02:22 +1100

    Move search client into it's own file

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react';
-import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, Configure, Hits } from 'react-instantsearch-dom';
 import { Results } from './search/results';
 import { SearchHit } from './search/search-hit';
 import { Query } from './search/query';
 import { SearchBox } from './search/search-box';
 import { ResultsWrapper } from './search/results-wrapper';
-
-const searchClient = algoliasearch(
-  'V259T7DYJV',
-  '9fc1d234897020d7151c5a2afeab15e7'
-);
+import { algoliaClient } from '@lib/algolia-client';
 
 function Searchbar() {
   return (
     <InstantSearch
       indexName="products_recently_ordered_count_desc"
-      searchClient={searchClient}
+      searchClient={algoliaClient}
     >
       <Configure hitsPerPage={6} />
       <SearchBox />
