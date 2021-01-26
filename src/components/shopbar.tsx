82eeac2b97ea1be03ab56885329888f14c8b2302
Luke Bennett <hello@lukebennett.com.au>
2021-01-26 12:05:26 +1100

    Style for mobile

--- src/components/shopbar.tsx
+++ src/components/shopbar.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
 import { signIn, signOut, useSession } from 'next-auth/client';
 
@@ -7,27 +6,22 @@ import { Container } from './container';
 import { HorizontalPadding } from './horizontal-padding';
 import { CartIcon, SignInIcon } from './vectors';
 import { Searchbar } from './search-bar';
-import { useCartCount } from '@lib/index';
+import { useCart } from '@lib/index';
+import { Logo } from './vectors/logo';
 
 function Shopbar() {
   const [session] = useSession();
-  const count = useCartCount();
+  const cart = useCart();
   return (
     <div className="font-bold bg-white">
       <Container>
         <HorizontalPadding>
           <div className="flex items-center justify-between">
-            <div className="w-48 transform -translate-y-6">
+            <div className="relative z-10 py-3 mr-4 transform sm:py-0 sm:-translate-y-6">
               <Link href="/">
-                <a className="block">
-                  <Image
-                    src="/logo.png"
-                    width={200}
-                    height={107}
-                    layout="intrinsic"
-                    alt="Growers Market"
-                    className="drop-shadow"
-                  />
+                <a className="block h-16 sm:h-24">
+                  <span className="sr-only">Growers Market</span>
+                  <Logo className="h-full drop-shadow" />
                 </a>
               </Link>
             </div>
@@ -40,10 +34,10 @@ function Shopbar() {
                     e.preventDefault();
                     signOut();
                   }}
-                  className="flex items-center space-x-2"
+                  className="relative flex flex-col items-center text-xs leading-none text-center sm:flex-row sm:space-x-2"
                 >
                   <SignInIcon className="w-7 h-7" />
-                  <span className="text-xs leading-tight">
+                  <span className="mt-1">
                     Sign <br />
                     Out
                   </span>
@@ -55,27 +49,18 @@ function Shopbar() {
                     e.preventDefault();
                     signIn(null, { callbackUrl: 'http://localhost:3000/' });
                   }}
-                  className="flex items-center space-x-2"
+                  className="relative flex flex-col items-center text-xs leading-none text-center sm:flex-row sm:space-x-2"
                 >
                   <SignInIcon className="w-7 h-7" />
-                  <span className="text-xs leading-tight">
-                    Sign <br />
-                    In
-                  </span>
+                  <span className="mt-1">Sign In</span>
                 </a>
               )}
               <Link href="/cart">
-                <a className="relative flex items-center space-x-2">
+                <a className="relative flex flex-col items-center text-xs leading-none text-center sm:flex-row sm:space-x-2">
                   <CartIcon className="w-7 h-7" />
-                  <span className="text-xs leading-tight">
-                    Your <br />
-                    Cart
+                  <span className="mt-1">
+                    ${cart?.totalPrice ? cart.totalPrice : '0.00'}
                   </span>
-                  {count > 0 ? (
-                    <span className="absolute top-0 right-0 flex items-center justify-center w-4 h-4 text-xs text-white transform translate-x-full -translate-y-1/2 rounded-full bg-green-dark">
-                      {count}
-                    </span>
-                  ) : null}
                 </a>
               </Link>
             </div>
