7370f934d89e0486e68cbf1a54f38e1ac4c7ac35
Luke Bennett <hello@lukebennett.com.au>
2021-01-28 13:23:14 +1100

    Comment out unused code

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -18,16 +18,16 @@ function Layout({ children }: LayoutProps) {
   );
 }
 
-function Breakpoint() {
-  return (
-    <div className="fixed bottom-0 w-full px-4 py-3 mx-auto font-bold text-center bg-white border-t shadow max-w-screen-2xl sm:px-6 lg:px-8">
-      <div className="sm:hidden">Mobile</div>
-      <div className="hidden sm:block md:hidden">SM</div>
-      <div className="hidden md:block lg:hidden">MD</div>
-      <div className="hidden lg:block xl:hidden">LG</div>
-      <div className="hidden xl:block">XL</div>
-    </div>
-  );
-}
+// function Breakpoint() {
+//   return (
+//     <div className="fixed bottom-0 w-full px-4 py-3 mx-auto font-bold text-center bg-white border-t shadow max-w-screen-2xl sm:px-6 lg:px-8">
+//       <div className="sm:hidden">Mobile</div>
+//       <div className="hidden sm:block md:hidden">SM</div>
+//       <div className="hidden md:block lg:hidden">MD</div>
+//       <div className="hidden lg:block xl:hidden">LG</div>
+//       <div className="hidden xl:block">XL</div>
+//     </div>
+//   );
+// }
 
 export { Layout };
