067aaab16ac93da7c69cbeade5c951eff4066dd7
Luke Bennett <hello@lukebennett.com.au>
2021-02-05 17:01:37 +1100

    Add skip nav

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { Header } from './header';
 import { Footer } from './footer';
+import { SkipNavLink, SkipNavContent } from '@reach/skip-nav';
 
 interface ILayout {
   children: React.ReactNode;
@@ -9,8 +10,17 @@ interface ILayout {
 
 function Layout({ children }: ILayout) {
   return (
-    <div className="flex flex-col min-h-screen font-sans antialiased bg-white text-gray-dark fill-available">
+    <div className="relative flex flex-col min-h-screen font-sans antialiased bg-white text-gray-dark fill-available">
+      <SkipNavLink
+        contentId="main-content"
+        className="absolute left-0 ml-4 font-bold border border-white focus:z-30 bg-yellow text-green-dark top-4 sm:ml-6 lg:ml-8"
+      >
+        <div className="px-4 py-2 border border-dashed border-green-dark">
+          Skip Nav
+        </div>
+      </SkipNavLink>
       <Header />
+      <SkipNavContent id="main-content" />
       <main className="flex-1">{children}</main>
       <Footer />
       {/* <Breakpoint /> */}
