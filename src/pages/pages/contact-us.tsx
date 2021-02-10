e6cc4a74fc4db6690b74a8a7da0cbd8017caca4d
Luke Bennett <hello@lukebennett.com.au>
2021-02-10 16:18:18 +1100

    Add contact page

---
+++ src/pages/pages/contact-us.tsx
@@ -0,0 +1,106 @@
+import { ContactForm } from '@components/contact-form';
+import {
+  Breadcrumbs,
+  Carousel,
+  Container,
+  DeliverySchedule,
+  HorizontalPadding,
+  TopSellingProducts,
+} from '@components/index';
+import { getSiteNavigation, getSiteSettings, getTopSelling } from '@lib/index';
+import { config } from 'config';
+import { GetStaticPropsResult } from 'next';
+import Link from 'next/link';
+import { NextSeo } from 'next-seo';
+import * as React from 'react';
+import { HiChevronLeft } from 'react-icons/hi';
+
+function ContactPage({ topSelling }): React.ReactElement {
+  // Navigation array from Breadcrumbs
+  const navigation = [
+    {
+      title: 'Contact Us',
+      handle: '/pages/contact-us',
+    },
+  ];
+
+  return (
+    <>
+      <NextSeo title="Contact Us" />
+      <Carousel />
+      <Breadcrumbs navigation={navigation} />
+      <Container>
+        <div className="relative grid lg:grid-cols-3">
+          <div className="py-16 lg:col-span-2">
+            <HorizontalPadding>
+              <div className="grid gap-8 lg:grid-cols-2">
+                <div>
+                  <h1 className="text-2xl font-bold">Send a Message</h1>
+                  <div className="-mt-6">
+                    <ContactForm />
+                  </div>
+                </div>
+                <div>
+                  <h1 className="text-2xl font-bold">Contact Us</h1>
+                  <dl className="mt-2 space-y-2">
+                    <div>
+                      <dt className="inline font-bold">Phone: </dt>
+                      <dd className="inline">{config.phone}</dd>
+                    </div>
+                    <div>
+                      <dt className="inline font-bold">Fax: </dt>
+                      <dd className="inline">{config.phone}</dd>
+                    </div>
+                    <div>
+                      <dt className="inline font-bold">Address: </dt>
+                      <dd className="inline">
+                        {config.address.line1}, {config.address.line2}
+                      </dd>
+                    </div>
+                    <div>
+                      <dt className="inline font-bold">Hours: </dt>
+                      <dd className="inline">
+                        {config.hours.weekdays} <br />
+                        {config.hours.weekends}
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
+              </div>
+            </HorizontalPadding>
+          </div>
+          <TopSellingProducts topSelling={topSelling} />
+        </div>
+        <DeliverySchedule />
+      </Container>
+    </>
+  );
+}
+
+interface ContactPageProps {
+  siteNavigation: [];
+  siteSettings: Record<string, unknown>;
+  topSelling: [];
+}
+
+async function getStaticProps(): Promise<
+  GetStaticPropsResult<ContactPageProps>
+> {
+  const topSelling = await getTopSelling({
+    query: 'available_for_sale:true',
+  });
+
+  const siteNavigation = await getSiteNavigation();
+  const siteSettings = await getSiteSettings();
+
+  return {
+    props: {
+      siteNavigation,
+      siteSettings,
+      topSelling,
+    },
+    revalidate: 60,
+  };
+}
+
+export { ContactPage as default, getStaticProps };
