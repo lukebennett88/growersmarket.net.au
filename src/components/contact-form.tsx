f02110e0224e8f444b5cd7b8fb97e0f5d7dba6ad
Luke Bennett <hello@lukebennett.com.au>
2021-02-10 20:49:11 +1100

    Send emails with nodemailer and Postmark

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -1,12 +1,50 @@
+/* eslint-disable promise/catch-or-return */
 import { Input, Textarea } from '@components/form-elements';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 import { useForm } from 'react-hook-form';
 
+interface DataProps {
+  email_address: string;
+  full_name: string;
+  message: string;
+  subject: string;
+}
+
 function ContactForm() {
   const { handleSubmit, register, errors } = useForm({ mode: 'onBlur' });
+
   const [isSubmitting, setIsSubmitting] = React.useState(false);
+
+  const router = useRouter();
+
+  const onSubmit = async (data: DataProps, event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    setIsSubmitting(true);
+
+    const { email_address, full_name, message, subject } = data;
+
+    fetch('/api/post-contact-form', {
+      method: 'POST',
+      body: JSON.stringify({
+        email_address,
+        full_name,
+        message,
+        subject,
+      }),
+    })
+      .then(() => router.push('/success'))
+      // eslint-disable-next-line no-console
+      .catch((error) => console.error(error))
+      .finally(() => setIsSubmitting(false));
+  };
+
   return (
-    <form className="grid max-w-xl gap-4 mt-8">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="grid max-w-xl gap-4 mt-8"
+    >
       <Input
         name="full_name"
         label="Full Name"
@@ -37,7 +75,7 @@ function ContactForm() {
         className="w-full border-gray-300 focus:ring-2 focus:ring-blue-500 focus-within:ring-opacity-50"
       />
       <div>
-        <button type="submit" className="cta">
+        <button type="submit" disabled={isSubmitting} className="cta">
           Submit
         </button>
       </div>
---
+++ src/pages/api/post-contact-form.ts
@@ -0,0 +1,55 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nodemailer from 'nodemailer';
+
+const postmarkTransport = require('nodemailer-postmark-transport');
+
+interface DataProps {
+  email_address: string;
+  full_name: string;
+  message: string;
+  subject: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { body } = req;
+
+  const data: DataProps = JSON.parse(body);
+
+  const transport = nodemailer.createTransport(
+    postmarkTransport({
+      auth: {
+        apiKey: process.env.POSTMARK_SERVER_API_TOKEN,
+      },
+    })
+  );
+
+  const mail = {
+    from: `services@phirannodesigns.com.au`,
+    to: `services@phirannodesigns.com.au`,
+    subject: `New Growers Market Contact Form Submission from ${data.full_name}`,
+    html: `<div>
+    <h1>New Growers Market Contact Form Submission</h1>
+    <ul>
+      <li>Name: ${data.full_name}</li>
+      <li>Email: ${data.email_address}</li>
+      <li>Subject: ${data.subject}</li>
+      <li>Message: ${data.message}</li>
+    </ul>
+  </div>`,
+  };
+
+  // Send the email
+  try {
+    const info = await transport.sendMail(mail);
+    console.log(info);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return res.status(200).json({
+    message: 'Success',
+  });
+}
