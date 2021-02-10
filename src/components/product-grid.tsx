08000ba8d6f89740202a5604cc6e10f025d1a5e0
Luke Bennett <hello@lukebennett.com.au>
2021-02-10 18:44:35 +1100

    Update ProductGrid

--- src/components/product-grid.tsx
+++ src/components/product-grid.tsx
@@ -1,7 +1,7 @@
 import { Container } from './container';
 import { HorizontalPadding } from './horizontal-padding';
 
-enum Variant {
+enum Colour {
   GRAY,
   WHITE,
 }
@@ -10,34 +10,38 @@ interface IProductGrid {
   heading?: string;
   children: React.ReactNode;
   columns?: 2 | 3 | 4;
-  variant?: Variant;
+  colour?: Colour;
 }
 
-const COLUMNS_MAPS = {
+const COLUMNS_MAP = {
   2: 'lg:grid-cols-2',
   3: 'lg:grid-cols-3',
   4: 'lg:grid-cols-4',
 };
 
-const VARIANT_MAPS = {
-  [Variant.GRAY]: HorizontalPadding.variant.GRAY,
-  [Variant.WHITE]: HorizontalPadding.variant.WHITE,
+const COLOUR_MAP = {
+  [Colour.GRAY]: 'bg-gray-light',
+  [Colour.WHITE]: 'bg-white',
 };
 
 function ProductGrid({
   heading,
   children,
   columns = 4,
-  variant = Variant.WHITE,
+  colour = Colour.WHITE,
 }: IProductGrid) {
   return (
-    <article className={columns === 4 ? 'lg:col-span-2' : null}>
+    <article
+      className={`${columns === 4 ? 'lg:col-span-2 ' : ''}${
+        COLOUR_MAP[colour]
+      }`}
+    >
       <Container>
-        <HorizontalPadding variant={VARIANT_MAPS[variant]}>
-          <div className="py-16 font-bold">
-            {heading || <h2 className="text-2xl">{heading}</h2>}
+        <HorizontalPadding>
+          <div className={`${columns !== 3 ? 'py-16 ' : ''}font-bold`}>
+            {heading && <h2 className="text-2xl">{heading}</h2>}
             <ul
-              className={`grid gap-12 mt-8 sm:grid-cols-2 ${COLUMNS_MAPS[columns]}`}
+              className={`grid gap-12 mt-8 sm:grid-cols-2 ${COLUMNS_MAP[columns]}`}
             >
               {children}
             </ul>
@@ -48,6 +52,6 @@ function ProductGrid({
   );
 }
 
-ProductGrid.variant = Variant;
+ProductGrid.colour = Colour;
 
 export { ProductGrid };
