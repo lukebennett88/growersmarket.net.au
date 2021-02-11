a88e8e2a0fb14406675a519c18776a958efab583
JD Manabat <manabat.johnder@gmail.com>
2021-02-11 12:43:08 +0800

    Site Navigation

--- src/lib/get-site-navigation.ts
+++ src/lib/get-site-navigation.ts
@@ -1,6 +1,7 @@
 import { gql } from '@apollo/client';
 
 import { apolloClient } from './apollo-client';
+import { getAllCollectionsByType } from './get-all-collections-by-type';
 
 const SANITY_DATA = gql`
   query SanityQuery {
@@ -21,6 +22,27 @@ const SANITY_DATA = gql`
   }
 `;
 
+function filterCollection(allProducts) {
+  const collections = allProducts.map(({ node }) => {
+    const collection = node.collections?.edges?.[0];
+    return JSON.stringify(collection);
+  });
+
+  const unique = [...new Set(collections)]
+    .filter((node) => typeof node === 'string')
+    .map((node: string) => JSON.parse(node));
+
+  const filterSpecials = unique.filter(
+    (collection) => collection.node.title !== 'Specials'
+  );
+
+  return filterSpecials.map((collection) => ({
+    route: collection.node.handle,
+    title: collection.node.title,
+    _key: collection.node.id,
+  }));
+}
+
 async function getSiteNavigation() {
   const { data } = await apolloClient.query({
     query: SANITY_DATA,
@@ -28,6 +50,47 @@ async function getSiteNavigation() {
       clientName: 'SANITY',
     },
   });
+
+  const fruitProducts = await getAllCollectionsByType({
+    query: `product_type:Fruit`,
+  });
+  const vegetablesProducts = await getAllCollectionsByType({
+    query: `product_type:Vegetables`,
+  });
+  const fridgeAndPantryProducts = await getAllCollectionsByType({
+    query: `product_type:Fridge & Pantry`,
+  });
+  const prePackedBoxesProducts = await getAllCollectionsByType({
+    query: `product_type:Pre-Packed Boxes`,
+  });
+
+  const siteProductNavigation = [
+    {
+      _key: '61997361e278',
+      title: 'Fruit',
+      route: 'fruit',
+      subMenu: filterCollection(fruitProducts),
+    },
+    {
+      _key: '3bebc89df5e1',
+      title: 'Vegetables',
+      route: 'vegetables',
+      subMenu: filterCollection(vegetablesProducts),
+    },
+    {
+      _key: '3e9c56148601',
+      title: 'Fridge & Pantry',
+      route: 'fridge-and-pantry',
+      subMenu: filterCollection(fridgeAndPantryProducts),
+    },
+    {
+      _key: 'd4d25a3c8d2e',
+      title: 'Pre-Packed Boxes',
+      route: 'pre-packed-boxes',
+      subMenu: filterCollection(prePackedBoxesProducts),
+    },
+  ];
+
   return data.SiteNavigation;
 }
 
