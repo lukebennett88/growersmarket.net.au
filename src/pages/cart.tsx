d09f941f9792fc49af1cfb4bfe99a9536d1f265f
Luke Bennett <hello@lukebennett.com.au>
2021-02-12 13:14:33 +1100

    Build logic for delivery step

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -62,7 +62,7 @@ function CartPage({ topSelling }: { topSelling: ITopSellingProducts }) {
               <ProgressIndicator step={step} setStep={setStep} />
               {step === 1 && <Summary setStep={setStep} />}
               {step === 2 && <Login setStep={setStep} authUser={authUser} />}
-              {step === 3 && <Delivery />}
+              {step === 3 && <Delivery setStep={setStep} />}
               {step === 4 && <ConfirmOrder />}
             </HorizontalPadding>
           </div>
@@ -283,9 +283,11 @@ function Login({ setStep, authUser }: ILogin) {
   );
 }
 
-function Delivery() {
+function Delivery({ setStep }) {
+  const nextStep = () => setStep(4);
   const [active, setActive] = React.useState('');
   const [deliveryArea, setDeliveryArea] = React.useState('');
+  const [deliveryDate, setDeliveryDate] = React.useState('');
   return (
     <div className="grid gap-8">
       {/* Delivery or Pick Up */}
@@ -295,8 +297,10 @@ function Delivery() {
           <button
             type="button"
             onClick={() => setActive('PICKUP')}
-            className={`p-4 border rounded ${
-              active === 'PICKUP' ? 'bg-green-dark text-white' : 'bg-white'
+            className={`p-4 border rounded transition duration-150 ease-in-out ${
+              active === 'PICKUP'
+                ? 'bg-green-dark text-white'
+                : 'bg-white hover:border-green-dark'
             }`}
           >
             <h3 className="font-bold">Pick Up</h3>
@@ -305,8 +309,10 @@ function Delivery() {
           <button
             type="button"
             onClick={() => setActive('DELIVERY')}
-            className={`p-4 border rounded ${
-              active === 'DELIVERY' ? 'bg-green-dark text-white' : 'bg-white'
+            className={`p-4 border rounded transition duration-150 ease-in-out ${
+              active === 'DELIVERY'
+                ? 'bg-green-dark text-white'
+                : 'bg-white hover:border-green-dark'
             }`}
           >
             <h3 className="font-bold">Delivery</h3>
@@ -315,80 +321,204 @@ function Delivery() {
         </div>
       </div>
       {/* Your Address */}
-      {active === 'DELIVERY' && (
-        <div className="bg-gray-light">
-          <h2 className="py-4 font-bold text-center">Your Address</h2>
-          <div className="grid px-8 py-4 text-center border-t gap-y-4 gap-x-8">
-            <button
-              type="button"
-              onClick={() => setDeliveryArea('PORT_MACQUARIE')}
-              className={`p-4 border rounded ${
-                deliveryArea === 'PORT_MACQUARIE'
-                  ? 'bg-green-dark text-white'
-                  : 'bg-white'
-              }`}
-            >
-              <h3 className="font-bold">Port Macquarie</h3>
-              <p>Monday, Tuesday, Wednesday, Thursday, Friday</p>
-              <p>Please place your order before 10am for same day delivery.</p>
-            </button>
-            <button
-              type="button"
-              onClick={() => setDeliveryArea('WAUCHOPE')}
-              className={`p-4 border rounded ${
-                deliveryArea === 'WAUCHOPE'
-                  ? 'bg-green-dark text-white'
-                  : 'bg-white'
-              }`}
-            >
-              <h3 className="font-bold">Wauchope</h3>
-              <p>Monday, Wednesday, Friday</p>
-              <p>Please place your order before 10am for same day delivery.</p>
-            </button>
-            <button
-              type="button"
-              onClick={() => setDeliveryArea('LAURIETON')}
-              className={`p-4 border rounded ${
-                deliveryArea === 'LAURIETON'
-                  ? 'bg-green-dark text-white'
-                  : 'bg-white'
-              }`}
-            >
-              <h3 className="font-bold">
-                Laurieton / Lake Cathie / North Haven / Bonny Hills
-              </h3>
-              <p>Tuesday and Friday</p>
-              <p>Please place your order before 10am for same day delivery.</p>
-            </button>
-            <button
-              type="button"
-              onClick={() => setDeliveryArea('KEMPSEY')}
-              className={`p-4 border rounded ${
-                deliveryArea === 'KEMPSEY'
-                  ? 'bg-green-dark text-white'
-                  : 'bg-white'
-              }`}
-            >
-              <h3 className="font-bold">Kempsey/Crescent Head</h3>
-              <p>Friday</p>
-              <p>Please place your order before 10am for same day delivery.</p>
-            </button>
-            <button
-              type="button"
-              onClick={() => setDeliveryArea('LORD_HOWE_ISLAND')}
-              className={`p-4 border rounded ${
-                deliveryArea === 'LORD_HOWE_ISLAND'
-                  ? 'bg-green-dark text-white'
-                  : 'bg-white'
-              }`}
-            >
-              <h3 className="font-bold">Lord Howe Island</h3>
-              <p>Monday, Tuesday, Wednesday, Thursday, Friday</p>
-              <p>Please place your order before 10am for same day delivery.</p>
-            </button>
-          </div>
+
+      <div className="bg-gray-light">
+        <h2 className="py-4 font-bold text-center">Your Address</h2>
+        <div className="grid px-8 py-4 text-center border-t gap-y-4 gap-x-8">
+          <button
+            type="button"
+            onClick={() => setDeliveryArea('PORT_MACQUARIE')}
+            className={`p-4 border rounded transition duration-150 ease-in-out ${
+              deliveryArea === 'PORT_MACQUARIE'
+                ? 'bg-green-dark text-white'
+                : 'bg-white hover:border-green-dark'
+            }`}
+          >
+            <h3 className="font-bold">Port Macquarie</h3>
+            <p>Delivered on: Monday, Tuesday, Wednesday, Thursday, Friday.</p>
+            <p>
+              Please place your order before 10am the day prior to delivery.
+            </p>
+          </button>
+          <button
+            type="button"
+            onClick={() => setDeliveryArea('WAUCHOPE')}
+            className={`p-4 border rounded transition duration-150 ease-in-out ${
+              deliveryArea === 'WAUCHOPE'
+                ? 'bg-green-dark text-white'
+                : 'bg-white hover:border-green-dark'
+            }`}
+          >
+            <h3 className="font-bold">Wauchope</h3>
+            <p>Delivered on: Monday, Wednesday, Friday.</p>
+            <p>
+              Please place your order before 10am the day prior to delivery.
+            </p>
+          </button>
+          <button
+            type="button"
+            onClick={() => setDeliveryArea('LAURIETON')}
+            className={`p-4 border rounded transition duration-150 ease-in-out ${
+              deliveryArea === 'LAURIETON'
+                ? 'bg-green-dark text-white'
+                : 'bg-white hover:border-green-dark'
+            }`}
+          >
+            <h3 className="font-bold">
+              Laurieton / Lake Cathie / North Haven / Bonny Hills
+            </h3>
+            <p>Delivered on: Tuesday and Friday.</p>
+            <p>
+              Please place your order before 10am the day prior to delivery.
+            </p>
+          </button>
+          <button
+            type="button"
+            onClick={() => setDeliveryArea('KEMPSEY')}
+            className={`p-4 border rounded transition duration-150 ease-in-out ${
+              deliveryArea === 'KEMPSEY'
+                ? 'bg-green-dark text-white'
+                : 'bg-white hover:border-green-dark'
+            }`}
+          >
+            <h3 className="font-bold">Kempsey/Crescent Head</h3>
+            <p>Delivered on: Friday.</p>
+            <p>
+              Please place your order before 10am the day prior to delivery.
+            </p>
+          </button>
+          <button
+            type="button"
+            onClick={() => setDeliveryArea('LORD_HOWE_ISLAND')}
+            className={`p-4 border rounded transition duration-150 ease-in-out ${
+              deliveryArea === 'LORD_HOWE_ISLAND'
+                ? 'bg-green-dark text-white'
+                : 'bg-white hover:border-green-dark'
+            }`}
+          >
+            <h3 className="font-bold">Lord Howe Island</h3>
+            <p>Delivered on: Monday, Tuesday, Wednesday, Thursday, Friday.</p>
+            <p>
+              Please place your order before 10am the day prior to delivery.
+            </p>
+          </button>
         </div>
-      )}
+      </div>
+      {active === 'PICKUP' &&
+        (deliveryArea === 'PORT_MACQUARIE' ||
+          deliveryArea === 'WAUCHOPE' ||
+          deliveryArea === 'LAURIETON' ||
+          deliveryArea === 'KEMPSEY' ||
+          deliveryArea === 'LORD_HOWE_ISLAND') && (
+          <div className="bg-gray-light">
+            <h2 className="py-4 font-bold text-center">Your Address</h2>
+            <div className="grid px-8 py-4 text-center border-t gap-y-4 gap-x-8">
+              <button
+                type="button"
+                onClick={() => setDeliveryDate('MONDAY')}
+                className={`p-4 border rounded transition duration-150 ease-in-out ${
+                  deliveryDate === 'MONDAY'
+                    ? 'bg-green-dark text-white'
+                    : 'bg-white hover:border-green-dark'
+                }`}
+              >
+                <h3 className="font-bold">Monday</h3>
+                <p>15th February</p>
+              </button>
+              <button
+                type="button"
+                onClick={() => setDeliveryDate('TUESDAY')}
+                className={`p-4 border rounded transition duration-150 ease-in-out ${
+                  deliveryDate === 'TUESDAY'
+                    ? 'bg-green-dark text-white'
+                    : 'bg-white hover:border-green-dark'
+                }`}
+              >
+                <h3 className="font-bold">Tuesday</h3>
+                <p>16th February</p>
+              </button>
+              <button
+                type="button"
+                onClick={() => setDeliveryDate('WEDNESDAY')}
+                className={`p-4 border rounded transition duration-150 ease-in-out ${
+                  deliveryDate === 'WEDNESDAY'
+                    ? 'bg-green-dark text-white'
+                    : 'bg-white hover:border-green-dark'
+                }`}
+              >
+                <h3 className="font-bold">Wednesday</h3>
+                <p>17th February</p>
+              </button>
+              <button
+                type="button"
+                onClick={() => setDeliveryDate('THURSDAY')}
+                className={`p-4 border rounded transition duration-150 ease-in-out ${
+                  deliveryDate === 'THURSDAY'
+                    ? 'bg-green-dark text-white'
+                    : 'bg-white hover:border-green-dark'
+                }`}
+              >
+                <h3 className="font-bold">Thursday</h3>
+                <p>18th February</p>
+              </button>
+              <button
+                type="button"
+                onClick={() => setDeliveryDate('FRIDAY')}
+                className={`p-4 border rounded transition duration-150 ease-in-out ${
+                  deliveryDate === 'FRIDAY'
+                    ? 'bg-green-dark text-white'
+                    : 'bg-white hover:border-green-dark'
+                }`}
+              >
+                <h3 className="font-bold">Friday</h3>
+                <p>19th February</p>
+              </button>
+            </div>
+          </div>
+        )}
+      <div className="flex justify-between mt-16">
+        <Link href="/">
+          <a className="inline-flex items-center space-x-2 cta text-green-dark bg-yellow">
+            <svg
+              aria-hidden
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5 -ml-3"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Continue Shopping
+          </a>
+        </Link>
+
+        {((active === 'PICKUP' && deliveryArea !== '' && deliveryDate !== '') ||
+          (active === 'DELIVERY' && deliveryArea !== '')) && (
+          <button
+            type="button"
+            onClick={nextStep}
+            className="inline-flex items-center space-x-2 cta text-green-dark bg-yellow"
+          >
+            Next Step
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5 -mr-3"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        )}
+      </div>
     </div>
   );
 }
