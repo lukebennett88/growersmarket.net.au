e3386d6559ad69ac4de4309f2d64c9103885f0b2
Luke Bennett <hello@lukebennett.com.au>
2021-02-17 17:17:18 +1100

    Wrap HorizontalPadding in a Container

--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { HiChevronRight } from 'react-icons/hi';
 
+import { Container } from './container';
 import { HorizontalPadding } from './horizontal-padding';
 
 interface IPage {
@@ -18,42 +19,44 @@ function Breadcrumbs({ navigation }: IBreadcrumbs): React.ReactElement {
       className="flex py-2 text-white bg-green-dark"
       aria-label="Breadcrumb navigation"
     >
-      <HorizontalPadding>
-        <ol className="flex items-center space-x-4">
-          <li>
-            <div>
-              <Link href="/">
-                <a className="text-sm font-medium">Home</a>
-              </Link>
-            </div>
-          </li>
-          {navigation.map(
-            ({ title, handle }) =>
-              title &&
-              handle && (
-                <li key={handle}>
-                  <div className="flex items-center">
-                    <HiChevronRight className="flex-shrink-0 w-5 h-5" />
-                    {/* // TODO: Fix this link */}
-                    <Link href={`/${handle}`}>
-                      <a
-                        // The last item in the breadcrumb list should always be the current page
-                        aria-current={
-                          handle === navigation[navigation.length - 1].handle
-                            ? 'page'
-                            : null
-                        }
-                        className="ml-4 text-sm font-medium"
-                      >
-                        {title}
-                      </a>
-                    </Link>
-                  </div>
-                </li>
-              )
-          )}
-        </ol>
-      </HorizontalPadding>
+      <Container>
+        <HorizontalPadding>
+          <ol className="flex items-center space-x-4">
+            <li>
+              <div>
+                <Link href="/">
+                  <a className="text-sm font-medium">Home</a>
+                </Link>
+              </div>
+            </li>
+            {navigation.map(
+              ({ title, handle }) =>
+                title &&
+                handle && (
+                  <li key={handle}>
+                    <div className="flex items-center">
+                      <HiChevronRight className="flex-shrink-0 w-5 h-5" />
+                      {/* // TODO: Fix this link */}
+                      <Link href={`/${handle}`}>
+                        <a
+                          // The last item in the breadcrumb list should always be the current page
+                          aria-current={
+                            handle === navigation[navigation.length - 1].handle
+                              ? 'page'
+                              : null
+                          }
+                          className="ml-4 text-sm font-medium"
+                        >
+                          {title}
+                        </a>
+                      </Link>
+                    </div>
+                  </li>
+                )
+            )}
+          </ol>
+        </HorizontalPadding>
+      </Container>
     </nav>
   );
 }
