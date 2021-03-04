8a25f24900401f68c81c28fe53b4aa07f8b7cb3c
Luke Bennett <hello@lukebennett.com.au>
2021-03-04 17:35:37 +1100

    Add submenu to mobile menu (#25)
    * Update mobile menu

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { DialogContent, DialogOverlay } from '@reach/dialog';
@@ -5,7 +6,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import * as React from 'react';
-import { HiMenu, HiX } from 'react-icons/hi';
+import { HiArrowLeft, HiChevronRight, HiMenu, HiX } from 'react-icons/hi';
 
 import siteNavigation from '../data/site-navigation.json';
 import siteSettings from '../data/site-settings.json';
@@ -53,7 +54,7 @@ function Topbar() {
               onClick={toggle}
               aria-controls="mobile-menu"
               aria-expanded={isOpen}
-              className="flex items-center p-2 mr-auto space-x-2 font-bold rounded-md md:hidden"
+              className="flex items-center p-2 mr-auto space-x-2 font-bold rounded-md lg:hidden"
             >
               <span>Menu</span>
               <HiMenu className="w-5 h-5" />
@@ -68,10 +69,18 @@ function Topbar() {
 
 function MobileMenu({ isOpen, setIsOpen }) {
   const router = useRouter();
-  const close = () => setIsOpen(false);
   const MotionDialogOverlay = motion.custom(DialogOverlay);
   const MotionDialogContent = motion.custom(DialogContent);
   const transition = { min: 0, max: 100, bounceDamping: 9 };
+  const [navigation, setNavigation] = React.useState(
+    siteNavigation.mainNavigation
+  );
+  const [productType, setProductType] = React.useState(null);
+  const close = () => {
+    setIsOpen(false);
+    setProductType(null);
+    setNavigation(siteNavigation.mainNavigation);
+  };
   return (
     <AnimatePresence>
       {isOpen && (
@@ -104,29 +113,62 @@ function MobileMenu({ isOpen, setIsOpen }) {
                 <span className="sr-only">Close sidebar</span>
               </button>
             </div>
-            <Link href="/">
-              <a className="flex items-center flex-shrink-0 px-4">
-                <Logo className="w-auto h-16" />
-                <span className="sr-only">Growers Market</span>
-              </a>
-            </Link>
+            <div className="grid items-center grid-cols-2">
+              <Link href="/">
+                <a className="px-4">
+                  <Logo className="w-auto h-16" />
+                  <span className="sr-only">Growers Market</span>
+                </a>
+              </Link>
+              {productType && (
+                <button
+                  type="button"
+                  aria-label="Back to browse categories"
+                  onClick={() => {
+                    setProductType(null);
+                    setNavigation(siteNavigation.mainNavigation);
+                  }}
+                  className="flex items-center space-x-2 font-medium text-white"
+                >
+                  <HiArrowLeft className="text-base" />
+                  <span aria-hidden>Back</span>
+                </button>
+              )}
+            </div>
             <div className="flex-1 h-0 mt-5 overflow-y-auto">
+              {productType && (
+                <Link href={productType.route}>
+                  <a
+                    onClick={close}
+                    className={`w-full flex items-center px-4 py-2 text-base font-medium text-white transition duration-150 ease-in-out border-l-4 border-transparent hover:border-yellow hover:bg-gray-50 hover:text-gray-900 group ${
+                      router.route === `/${String(productType.route)}`
+                        ? 'border-yellow bg-gray-50 text-gray-900'
+                        : ''
+                    }`}
+                  >
+                    View all {productType.title}
+                  </a>
+                </Link>
+              )}
               <nav className="flex flex-col h-full">
                 <div className="space-y-1">
-                  {siteNavigation.mainNavigation.map((navItem) => (
-                    <Link key={navItem.id} href={`/${navItem.route}`}>
-                      <a
-                        onClick={close}
-                        className={`flex items-center px-4 py-2 text-base font-medium text-white transition duration-150 ease-in-out border-l-4 border-transparent hover:border-yellow hover:bg-gray-50 hover:text-gray-900 group ${
-                          router.route === `/${navItem.route}`
-                            ? 'border-yellow bg-gray-50 text-gray-900'
-                            : ''
-                        }`}
-                      >
-                        {navItem.title}
-                      </a>
-                    </Link>
-                  ))}
+                  {navigation.map((navItem) =>
+                    navItem.subMenu ? (
+                      <NavButton
+                        key={navItem.id}
+                        navItem={navItem}
+                        setNavigation={setNavigation}
+                        setProductType={setProductType}
+                      />
+                    ) : (
+                      <NavLink
+                        key={navItem.id}
+                        navItem={navItem}
+                        productType={productType}
+                        close={close}
+                      />
+                    )
+                  )}
                 </div>
                 <div className="pt-10 mt-auto space-y-1">
                   <Link href="/account">
@@ -144,6 +186,52 @@ function MobileMenu({ isOpen, setIsOpen }) {
         </MotionDialogOverlay>
       )}
     </AnimatePresence>
+  );
+}
+
+function NavButton({ navItem, setNavigation, setProductType }) {
+  const router = useRouter();
+  const handleClick = () => {
+    setProductType(navItem);
+    setNavigation(navItem.subMenu);
+  };
+  return (
+    <button
+      type="button"
+      onClick={handleClick}
+      className={`w-full flex justify-between space-x-2 items-center px-4 py-2 text-base font-medium text-white transition duration-150 ease-in-out border-l-4 border-transparent hover:border-yellow hover:bg-gray-50 hover:text-gray-900 group ${
+        router.route === `/${String(navItem.route)}`
+          ? 'border-yellow bg-gray-50 text-gray-900'
+          : ''
+      }`}
+    >
+      <span>{navItem.title}</span>
+      <HiChevronRight />
+    </button>
+  );
+}
+
+function NavLink({ navItem, productType, close }) {
+  const router = useRouter();
+  return (
+    <Link
+      href={
+        productType
+          ? `/collections/${String(navItem.handle)}`
+          : `/${String(navItem.route)}`
+      }
+    >
+      <a
+        onClick={close}
+        className={`flex items-center px-4 py-2 text-base font-medium text-white transition duration-150 ease-in-out border-l-4 border-transparent hover:border-yellow hover:bg-gray-50 hover:text-gray-900 group ${
+          router.route === `/${String(navItem.route)}`
+            ? 'border-yellow bg-gray-50 text-gray-900'
+            : ''
+        }`}
+      >
+        {navItem.title}
+      </a>
+    </Link>
   );
 }
 
