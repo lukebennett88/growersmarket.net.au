a438d4d42948e7f082b983fe1fed5010eb1a6827
Luke Bennett <hello@lukebennett.com.au>
2021-04-16 08:48:29 +1000

    Check if env vars are working on prod

--- src/pages/api/reindex-in-algolia.ts
+++ src/pages/api/reindex-in-algolia.ts
@@ -2,13 +2,18 @@ import { getAllProductsForAlgolia } from '@lib/get-all-products-for-algolia';
 import algoliasearch from 'algoliasearch';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const {
+  NEXT_PUBLIC_ALGOLIA_APPLICATION_ID,
+  ALGOLIA_ADMIN_API_KEY,
+} = process.env;
+
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse
 ) {
   const client = algoliasearch(
-    process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID,
-    process.env.ALGOLIA_ADMIN_API_KEY
+    NEXT_PUBLIC_ALGOLIA_APPLICATION_ID,
+    ALGOLIA_ADMIN_API_KEY
   );
 
   const products = await getAllProductsForAlgolia();
@@ -20,8 +25,15 @@ export default async function handler(
     // eslint-disable-next-line no-console
     .then(({ objectIDs }) => console.log(objectIDs))
     // eslint-disable-next-line no-console
-    .catch((error) => console.error({ error }));
+    .catch((error) =>
+      error.status(405).json({
+        message: `Error:
+        ${String(error)}`,
+      })
+    );
   return res.status(200).json({
-    message: 'Success',
+    message: `Success:
+    NEXT_PUBLIC_ALGOLIA_APPLICATION_ID: ${typeof NEXT_PUBLIC_ALGOLIA_APPLICATION_ID}
+    ALGOLIA_ADMIN_API_KEY: ${typeof ALGOLIA_ADMIN_API_KEY}`,
   });
 }
