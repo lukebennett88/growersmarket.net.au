d97c63ac344325666963542d7ac15ab7ad70a94c
Luke Bennett <hello@lukebennett.com.au>
2021-05-13 09:06:11 +1000

    Add South West Rocks to delivery zones

--- src/components/cart/delivery-zone.tsx
+++ src/components/cart/delivery-zone.tsx
@@ -21,7 +21,7 @@ const deliveryZones = [
   },
   {
     zone: 'Kempsey',
-    heading: 'Kempsey/Crescent Head',
+    heading: 'Kempsey / Crescent Head / South West Rocks',
     deliveryDays: 'Friday.',
   },
   {
